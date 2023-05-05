d7693420180e0ef6cd53b24fbb00ac9a7c42bef8
adrian-soch <adr.soch@outlook.com>
2023-05-05 15:01:22 -0400

    Update consersion code

--- data_tools/src/pcd2xyzi.cpp
+++ data_tools/src/pcd2xyzi.cpp
@@ -1,97 +1,80 @@
-#include <pcl/io/pcd_io.h>
-#include <pcl/console/print.h>
-#include <pcl/console/parse.h>
-#include <pcl/console/time.h>
-#include "pcl/io/pcd_io.h"
-#include "pcl/common/common.h"
-
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <filesystem>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
 
-void printUsage(const char *progName)
-{
-    std::cout << "\nUse: " << progName << " <file> -o <output dir>" << std::endl
-              << "Support: .pcd" << std::endl
-              << "[q] to exit" << std::endl;
+// Use the std namespace
+using namespace std;
+
+// Define a function to read a .pcd file with the PCL library and return a point cloud pointer
+pcl::PointCloud<pcl::PointXYZI>::Ptr read_pcd(const string& filename) {
+    // Create an empty point cloud pointer
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+
+    // Load the .pcd file into the point cloud
+    if (pcl::io::loadPCDFile<pcl::PointXYZI>(filename, *cloud) == -1) {
+        // Print an error message if the file cannot be loaded
+        cerr << "Couldn't read file " << filename << endl;
+    }
+
+    // Return the point cloud pointer
+    return cloud;
 }
 
-int main(int argc, char **argv)
-{
-    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>());
-    std::vector<int> filenames;
-    bool file_is_pcd = false;
+// Define a function to write a point cloud as a xyzi text file
+void write_xyzi(const string& filename, const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud) {
+    // Open the file for writing
+    ofstream file(filename);
 
-    if (argc < 4 or argc > 4)
-    {
-        printUsage(argv[0]);
+    // Check if the file is open
+    if (file.is_open()) {
+        // Loop through each point in the point cloud
+        for (const auto& point : cloud->points) {
+            // Write the x, y, z, and i values of the point separated by whitespace
+            file << point.x << " " << point.y << " " << point.z << " " << point.intensity << endl;
+        }
+
+        // Close the file
+        file.close();
+    }
+}
+
+// Define the main function
+int main(int argc, char** argv) {
+    // Check if a directory is given as an argument
+    if (argc < 2) {
+        // Print an error message and exit
+        cerr << "Usage: " << argv[0] << " <directory>" << endl;
         return -1;
     }
 
-    pcl::console::TicToc tt;
-    pcl::console::print_highlight("Loading ...");
+    // Get the directory name from the argument
+    string dir = argv[1];
 
-    if (filenames.size() <= 0)
-    {
-        filenames = pcl::console::parse_file_extension_argument(argc, argv, ".pcd");
-        if (filenames.size() == 1)
-        {
-            file_is_pcd = true;
+    // Loop through each file in the directory
+    for (const auto& entry : filesystem::directory_iterator(dir)) {
+        // Get the file path and name
+        string path = entry.path().string();
+        string name = entry.path().filename().string();
+
+        // Check if the file has a .pcd extension
+        if (entry.path().extension() == ".pcd") {
+            // Read the .pcd file with the PCL library and get the point cloud pointer
+            pcl::PointCloud<pcl::PointXYZI>::Ptr cloud = read_pcd(path);
+
+            // Change the extension of the file name to .xyzi
+
+            path.replace(path.find(".pcd"), 4, ".txt");
+
+            // Write the point cloud as a xyzi text file with the same name
+            std::cout << path << std::endl;
+            write_xyzi(path, cloud);
         }
     }
-    else
-    {
-        printUsage(argv[0]);
-        return -1;
-    }
 
-    std::string output_dir;
-    if (pcl::console::parse_argument(argc, argv, "-o", output_dir) == -1)
-    {
-        PCL_ERROR("Need an output directory! Please use <input cloud> -o <output dir>\n");
-        return (-1);
-    }
-
-    if (file_is_pcd)
-    {
-        if (pcl::io::loadPCDFile(argv[filenames[0]], *cloud) < 0)
-        {
-            std::cout << "Error loading point cloud " << argv[filenames[0]] << "\n";
-            printUsage(argv[0]);
-            return -1;
-        }
-        pcl::console::print_info("\nFound pcd file.\n");
-        pcl::console::print_info("Done, (");
-        pcl::console::print_value("%g", tt.toc());
-        pcl::console::print_info(" ms : ");
-        pcl::console::print_value("%d", cloud->size());
-        pcl::console::print_info(" points)\n");
-    }
-    else
-    {
-        printUsage(argv[0]);
-        return -1;
-    }
-
-    cloud->width = (int)cloud->points.size();
-    cloud->height = 1;
-    cloud->is_dense = true;
-
-    std::ofstream fout;
-
-    std::string str1 = output_dir;
-    str1 += "/txt_cloud.xyzi";
-    fout.open(str1.c_str());
-
-    for (int i = 0; i < cloud->points.size(); ++i)
-    {
-        fout << cloud->points[i].x << " " << cloud->points[i].y << " " << cloud->points[i].z << " " << cloud->points[i].intensity << std::endl;
-    }
-
-    fout.close();
-
-    pcl::console::print_info("Data saved");
-    std::cout << std::endl;
-
+    // Return 0 to indicate success
     return 0;
 }
