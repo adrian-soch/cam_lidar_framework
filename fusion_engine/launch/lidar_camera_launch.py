e4cd7109cc71b1b2633a158ecd2799b7c6b6b464
adrian-soch <adr.soch@outlook.com>
2023-05-10 14:03:05 -0400

    Update launch files

--- fusion_engine/launch/lidar_camera_launch.py
+++ fusion_engine/launch/lidar_camera_launch.py
@@ -1,30 +1,23 @@
 import os
 
-from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
-PACKAGE = 'fusion_engine'
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     # Launch the Brio camera
-    brio_camera = ExecuteProcess(
-        cmd=[[
-            'ros2 run image_tools cam2image --ros-args -p ',
-            'device_id:=2 ',
-            '-p frequency:=10.0 ',
-            '-p width:=1920 ',
-            '-p height:=1080'
-        ]],
-        shell=True
-    )
+    brio_camera = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            FindPackageShare("cam2image"),
+            "camera_launch.py")])
+        )
     
     # Add the LiDAR launch file to this launch
     ouster_lidar = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory(PACKAGE),
+            FindPackageShare("fusion_engine"),
             'ouster_driver_launch.py')])
         )
     # Items above will only be launched if they are present in this list
