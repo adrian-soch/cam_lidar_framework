461defaa509549f95a05736060516e9e34051a90
adrian-soch <adr.soch@outlook.com>
2023-05-24 16:38:29 -0400

    Update launch

--- lidar_pipeline/launch/full_demo.launch.py
+++ lidar_pipeline/launch/full_demo.launch.py
@@ -1,11 +1,20 @@
+import os
+
 from launch_ros.actions import Node
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 from launch.substitutions import PathJoinSubstitution
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from ament_index_python.packages import get_package_share_directory
 
 from launch_ros.substitutions import FindPackageShare
+
+'''
+Rosbag and param file must match for proper functionality
+example: 
+    bag: q7_2_may10_2023
+    data_dependant_params file: may10_config.yaml
+'''
 
 ABS_PATH_TO_ROSBAGS = '/home/adrian/dev/bags/'
 # BAG_NAME = 'dec7_2022/roofTestDark_1_HD_qosOverrride_true/'
@@ -13,7 +22,22 @@ ABS_PATH_TO_ROSBAGS = '/home/adrian/dev/bags/'
 # BAG_NAME = 'may10_2023/q6_2_may10_2023'
 BAG_NAME = 'may10_2023/q7_2_may10_2023'
 
+share_dir = get_package_share_directory('lidar_pipeline')
+pipeline_params = os.path.join(share_dir, 'configs', 'lidar_pipeline_config.yaml')
+data_dependant_params = os.path.join(share_dir, 'configs', 'may10_config.yaml')
+
 def generate_launch_description():
+
+    perception_node = Node(
+            package='lidar_pipeline',
+            executable='perception_node',
+            name='perception_node',
+            output='screen',
+            parameters=[
+                pipeline_params,
+                data_dependant_params
+            ]
+    )
 
     s_transform = Node(
             package='tf2_ros',
@@ -46,14 +70,6 @@ def generate_launch_description():
             name='tracker_bbox_viz',
             output='screen',
         )
-
-    perception_node = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution(
-                [FindPackageShare("lidar_pipeline"), "launch",
-                    "processing_and_transform_launch.py"])]
-        )
-    )
 
     rosbag_play = ExecuteProcess(
         cmd=[[
