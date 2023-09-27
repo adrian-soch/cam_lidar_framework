2308b6d306e0afa0c8512c5b3eb72f2f2317e4a3
adrian-soch <adr.soch@outlook.com>
2023-09-27 17:58:55 -0400

    Limit cpu usage

--- obj_tracker/obj_tracker/obj_tracker.py
+++ obj_tracker/obj_tracker/obj_tracker.py
@@ -14,6 +14,12 @@
  * 
  * @copyright Copyright (c) 2023
 """
+import os
+os.environ["OMP_NUM_THREADS"] = "1" 
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1" 
 
 from .obj_tracker_utils import *
 
