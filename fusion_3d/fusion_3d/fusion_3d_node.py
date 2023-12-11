b480e12cbc3b57f171b8e2fb4d19c85e5aba0102
adrian-soch <adr.soch@outlook.com>
2023-12-11 16:18:15 -0500

    Add fusion 3d package

---
+++ fusion_3d/fusion_3d/fusion_3d_node.py
@@ -0,0 +1,198 @@
+#! /usr/bin/env python3
+"""
+@file fusion_3d_node.py
+
+@brief This node subscribes to 3D detections/tracks from camera and lidar sensors
+    It will associate dets based on euclidean distance between 3D centroids.
+    Unmatched dets + matched dets list will be tracked via modified SORT MOT tracker.
+    Final results will be published as a Detection3DArray.
+
+@section Author(s)
+- Created by Adrian Sochaniwsky on 25/09/2023
+"""
+
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
+from fusion_module.utils import createDetection3DArr, detection3DArray2Numpy
+from fusion_module.sort_centroid import Sort, centroid_distance_batch, linear_assignment
+from vision_msgs.msg import Detection3DArray
+from rclpy.node import Node
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+import rclpy
+
+import numpy as np
+import time
+
+
+class DetectionSyncNode(Node):
+    def __init__(self):
+        super().__init__('detection_sync_node')
+
+        self.world_frame = self.declare_parameter(
+            'world_frame', 'map').get_parameter_value().string_value
+
+        cam_track_topic = self.declare_parameter(
+            'cam_track_topic', 'image_proc/det3D_tracks').get_parameter_value().string_value
+        lidar3d_track_topic = self.declare_parameter(
+            'lidar3d_track_topic', 'lidar_proc/tracks').get_parameter_value().string_value
+        
+        tracker_dist_thresh = self.declare_parameter(
+            'tracker_dist_thresh', 3.0).get_parameter_value().double_value
+        
+        self.fusion_dist_thresh = self.declare_parameter(
+            'fusion_dist_thresh', 3.0).get_parameter_value().double_value
+
+        self.lidar_only_override = self.declare_parameter(
+            'lidar_only_override', False).get_parameter_value().bool_value
+
+        self.camera_only_override = self.declare_parameter(
+            'camera_only_override', False).get_parameter_value().bool_value
+
+        if self.lidar_only_override and self.camera_only_override:
+            self.get_logger().error('Sensor overrrides are mutally exclusive, check parameters.')
+            exit(-1)
+
+        # Create subscribers and the approximate syncronizer message filter
+        cam_sub = Subscriber(self, Detection3DArray, cam_track_topic)
+        lidar_sub = Subscriber(self, Detection3DArray, lidar3d_track_topic)
+        sync = ApproximateTimeSynchronizer(
+            [cam_sub, lidar_sub], queue_size=10, slop=0.030)
+        sync.registerCallback(self.callback)
+
+        # Create SORT instance for fused detections
+        self.tracker = Sort(max_age=2, min_hits=3, dist_threshold=tracker_dist_thresh)
+
+        # Create publisher
+        self.track_publisher_ = self.create_publisher(
+            Detection3DArray, 'image_proc/fusion_3d_tracks', 2)
+
+        self.get_logger().info('Fusion Module initialized.')
+
+    def callback(self, cam_3d_dets: Detection3DArray, lidar_3d_dets: Detection3DArray):
+        """This callback will take the 2 sensors detection arrays and produce a final fused and tracked array
+
+        Args:
+            cam_3d_dets (Detection3DArray): Cam detections/tracks
+            lidar_3d_dets (Detection3DArray): LiDAR detections/tracks
+        """
+        t1 = time.clock_gettime(time.CLOCK_THREAD_CPUTIME_ID)
+
+        # Convert message arrays to numpy
+        cam_dets = detection3DArray2Numpy(
+            cam_3d_dets.detections, sensor_type='C')
+        lidar_dets = detection3DArray2Numpy(
+            lidar_3d_dets.detections, sensor_type='L')
+        
+        if self.camera_only_override:
+            fused_detections = cam_dets
+        elif self.lidar_only_override:
+            fused_detections = lidar_dets
+        else:
+            # Simple fusion with IoU based association
+            fused_detections = self.fuse(
+                cam_arr=cam_dets, lidar_arr=lidar_dets, dist_threshold=self.fusion_dist_thresh)
+
+        # Update SORT with detections
+        track_ids = self.tracker.update(fused_detections)
+
+        # Create and Publish 3D Detections with Track IDs
+        track_msg_arr = createDetection3DArr(
+            track_ids, cam_3d_dets.header)
+        self.track_publisher_.publish(track_msg_arr)
+
+        # Get and publish the execution time
+        t2 = time.clock_gettime(time.CLOCK_THREAD_CPUTIME_ID)
+        self.get_logger().info('Tracked {:4d} objects in {:.1f} msec.'.format(
+            len(track_msg_arr.detections), (t2-t1)*1000))
+
+    def fuse(self, cam_arr, lidar_arr, dist_threshold=0.3) -> np.ndarray:
+        """Perform late fusion between 2 sets of Axis-aligned bounding boxes from 2 different sensors
+
+        Associate the 2 lists. For all matched detections just keep the camera detection.
+        Keep all unmatched detections. This will use all camera detections
+        including the longer distance and also use lidar detections in
+        poor conditions the camera didnt get
+
+        NOTE: Code is adapted from SORT algorithm
+
+        Args:
+            cam_arr (np.ndarray): Cam detection array
+            lidar_arr (np.ndarray): Lidar detection array
+            dist_threshold (float, optional): Threshold for euclidean-based
+                association. Defaults to 3.0 m.
+
+        Returns:
+            np.ndarray: list of fused filt_match_inds and unmatched detection from both sensors
+        """
+
+        # Get NxN matrix of IoU between detections
+        iou_matrix = centroid_distance_batch(cam_arr, lidar_arr)
+
+        # Find all filt_match_inds
+        if min(iou_matrix.shape) > 0:
+            a = (iou_matrix > dist_threshold).astype(np.int32)
+            if a.sum(1).max() == 1 and a.sum(0).max() == 1:
+                matched_indices = np.stack(np.where(a), axis=1)
+            else:
+                matched_indices = linear_assignment(-iou_matrix)
+        else:
+            matched_indices = np.zeros(shape=(0, 2))
+
+        # Get unmached camera detections
+        unmatched_cam_dets_inds = []
+        for d in range(len(cam_arr)):
+            if (d not in matched_indices[:, 0]):
+                unmatched_cam_dets_inds.append(d)
+
+        # Get unmatched lidar detections
+        unmatched_lidar_dets_inds = []
+        for d in range(len(lidar_arr)):
+            if (d not in matched_indices[:, 1]):
+                unmatched_lidar_dets_inds.append(d)
+
+        # Filter out matched with low IOU
+        filt_match_inds = []
+        for m in matched_indices:
+            if (iou_matrix[m[0], m[1]] < dist_threshold):
+                unmatched_cam_dets_inds.append(m[0])
+                unmatched_lidar_dets_inds.append(m[1])
+            else:
+                filt_match_inds.append(m.reshape(1, 2))
+        if (len(filt_match_inds) == 0):
+            filt_match_inds = np.empty((0, 2), dtype=int)
+        else:
+            filt_match_inds = np.concatenate(filt_match_inds, axis=0)
+
+        fused_dets = []
+        for i in filt_match_inds:
+            # Change sensor type to 'B' (both)
+            det = cam_arr[i[0], :]
+            det[-1] = float(ord('B'))
+            fused_dets.append(cam_arr[i[0], :])
+
+        unmatched_cam_dets = []
+        for i in unmatched_cam_dets_inds:
+            unmatched_cam_dets.append(cam_arr[i, :])
+
+        unmatched_lidar_dets = []
+        for i in unmatched_lidar_dets_inds:
+            unmatched_lidar_dets.append(lidar_arr[i, :])
+
+        return np.array(fused_dets + unmatched_cam_dets + unmatched_lidar_dets)
+
+
+def main():
+    rclpy.init()
+    node = DetectionSyncNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ fusion_3d/fusion_3d/fusion_3d_visualizer_node.py
@@ -0,0 +1,129 @@
+#! /usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from vision_msgs.msg import Detection2DArray
+from cv_bridge import CvBridge
+from cv2 import flip, rectangle, putText, FONT_HERSHEY_PLAIN
+from cv2 import VideoWriter, VideoWriter_fourcc
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+
+import datetime
+
+
+class FusionVisualizer(Node):
+
+    def __init__(self):
+        super().__init__('fusion_visualizer')
+
+        self.declare_parameter('flip_image', True)
+        self.flip_image = self.get_parameter(
+            'flip_image').get_parameter_value().bool_value
+
+        self.declare_parameter('save_video', False)
+        save_video = self.get_parameter(
+            'save_video').get_parameter_value().bool_value
+        
+        self.declare_parameter('video_name', 'video')
+        self.video_name = self.get_parameter(
+            'video_name').get_parameter_value().string_value
+
+        self.bridge = CvBridge()
+        self.image_sub = Subscriber(self, Image, 'image')
+        self.detection_sub = Subscriber(
+            self, Detection2DArray, 'image_proc/fusion_tracks')
+        self.ats = ApproximateTimeSynchronizer(
+            [self.image_sub, self.detection_sub], queue_size=3, slop=0.1)
+        self.ats.registerCallback(self.callback)
+
+        self.image_pub = self.create_publisher(
+            Image, 'image_proc/fusion_2D_result', 2)
+
+        # BGR Colours
+        self.colour_codes = [(20, 230,  0),  # C - camera
+                             (0, 140, 255),  # L - lidar
+                             (255, 140, 0),  # B - both
+                             (0,  0, 202)]   # U - unknown
+
+        if save_video is True:
+            self.video_writer = None
+        else:
+            self.video_writer = False
+        self.video_writer_activated = False
+        self.get_logger().info('Fusion Visualizer started.')
+
+    def callback(self, image_msg, detection_msg):
+        # Convert the image message to OpenCV format
+        cv_image = self.bridge.imgmsg_to_cv2(
+            image_msg, desired_encoding='bgr8')
+
+        if self.flip_image is True:
+            cv_image = flip(cv_image, -1)
+
+        # Loop through the detections and draw them on the image
+        for detection in detection_msg.detections:
+            # Get the bounding box coordinates
+            try:
+                x = int(detection.bbox.center.x - detection.bbox.size_x / 2)
+                y = int(detection.bbox.center.y - detection.bbox.size_y / 2)
+                w = int(detection.bbox.size_x)
+                h = int(detection.bbox.size_y)
+            except ValueError:
+                continue
+
+            # Indicates which sensor contributed to this track result
+            colour = None
+            sensor_indicator = detection.results[0].hypothesis.class_id
+
+            if sensor_indicator == 'C':
+                # For camera only
+                colour = self.colour_codes[0]
+            elif sensor_indicator == 'L':
+                # For LiDAR only
+                colour = self.colour_codes[1]
+            elif sensor_indicator == 'B':
+                # For both sensors
+                colour = self.colour_codes[2]
+            else:
+                colour = self.colour_codes[3]
+
+            # Draw a rectangle around the detection
+            rectangle(cv_image, (x, y), (x + w, y + h), (40, 40, 250), 2)
+
+            # Get the track_id of the detection
+            track_id = int(detection.results[0].hypothesis.score)
+
+            # Draw the label above the rectangle
+            rectangle(cv_image, (x, y + h), (x + 55, y + h + 22), colour, -1)
+            putText(cv_image, f'{sensor_indicator}-{track_id}', (x, y +
+                    h + 20), FONT_HERSHEY_PLAIN, 1.3, (255, 255, 255), 2)
+
+        # Convert the image back to ROS format and publish it
+        image_with_detections_msg = self.bridge.cv2_to_imgmsg(
+            cv_image, encoding='bgr8')
+        self.image_pub.publish(image_with_detections_msg)
+
+        # create video writer if not exists
+        if self.video_writer is None:
+            fourcc = VideoWriter_fourcc(*'mp4v')  # choose codec
+            fps = 10  # choose frame rate
+            filename = self.video_name + '_{}.mp4'.format(datetime.datetime.now().strftime(
+                '%Y%m%d_%H%M%S'))  # choose file name with current timestamp
+            self.video_writer = VideoWriter(
+                filename, fourcc, fps, (cv_image.shape[1], cv_image.shape[0]))
+            self.video_writer_activated = True
+        elif self.video_writer_activated is True:
+            # write image to video file
+            self.video_writer.write(cv_image)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = FusionVisualizer()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
