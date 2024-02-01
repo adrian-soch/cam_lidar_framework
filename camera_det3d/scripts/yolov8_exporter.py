afb549e9c11bd852d8a5af780cb832b6ac809cec
adrian-soch <adr.soch@outlook.com>
2024-01-31 19:48:14 -0500

    Rename folder

--- camera_det3d/scripts/yolov8_exporter.py
+++ camera_det3d/scripts/yolov8_exporter.py
@@ -11,8 +11,8 @@ from ultralytics import YOLO
 
 # Load a model, if an offical model it will auto-download
 # if custom model, provide full path
-model = YOLO('yolov8m-seg.pt')
+model = YOLO('yolov8m.pt')
 
 # Export the model
 # see here for options: https://docs.ultralytics.com/modes/export/#arguments
-model.export(format='engine', imgsz=640, batch=1, half=True, simplify=False)
+model.export(format='engine', imgsz=640, batch=1, half=True, simplify=True)
