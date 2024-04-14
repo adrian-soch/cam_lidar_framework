14dff27f7f3ff0b064ddfbabe669c73cc29837eb
adrian-soch <adr.soch@outlook.com>
2024-04-13 21:10:01 -0400

    Add training script

--- learned_lidar_detector/train_yolov8_obb.py
+++ learned_lidar_detector/train_yolov8_obb.py
@@ -1,27 +1,8 @@
-# from ultralytics.models.yolo.obb import OBBTrainer
-
-# args = dict(model='yolov8n-obb.pt', data='a9_obb.yaml', epochs=1)
-# trainer = OBBTrainer(overrides=args)
-# trainer.train()
-
-
-
 from ultralytics import YOLO
-from torch.utils.data import DataLoader
 
-from a9_dataset import A9LidarBevCreator
-
-# Create a model from YAML file
-model = YOLO('yolov8n.yaml')
-
-# Load pretrained weights (optional)
-model.load('yolov8n.pt')
-
-train_dataloader = DataLoader(A9LidarBevCreator, batch_size=configs.batch_size, shuffle=(train_sampler is None),
-                                  pin_memory=configs.pin_memory, num_workers=configs.num_workers, sampler=train_sampler)
-
-# Create a custom dataloader object
-dataloader = CustomDataLoader(...)
+model = YOLO('yolov8n-obb.pt')  # load a pretrained model (recommended for training)
+# model = YOLO('yolov8n-obb.yaml')  # build a new model from YAML
+# model = YOLO('yolov8n-obb.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data=dataloader, epochs=100, imgsz=640)
+results = model.train(data='./configs/a9_bev.yaml', epochs=30, imgsz=1024, close_mosaic=0, batch=4)
